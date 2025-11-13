9045eee129608b3fd758482d8a983fadcaba6303
Mantas Ragaišis <ragaisis.m@gmail.com>
2025-11-13 22:10:55 +0200

    Add files via upload
    Sukelti visi programos failai.
    Sukurti run.bat ir CMakeLists.txt.

--- vector_list_testavimas.cpp
+++ vector_list_testavimas.cpp
@@ -18,12 +18,9 @@ void testuoti_konteinerius(int skaiciavimo_metodas) {
     // NAUJAS KODAS - Pasirinkti strategiją
     int strategija = pasirinkti_strategija();
 
-    // Failai testavimui
-    // vector<string> failai = {"stud1000.txt","stud10000.txt","stud100000.txt","stud1000000.txt"};
-    // vector<int> irasai = {1000,10000,100000,1000000};
-
-    vector<string> failai = {"stud1000000.txt"};
-    vector<int> irasai = {1000000};
+    //Failai testavimui
+    vector<string> failai = {"stud1000.txt","stud10000.txt","stud100000.txt","stud1000000.txt", "stud10000000.txt"};
+    vector<int> irasai = {1000,10000,100000,1000000, 10000000};
 
     if (konteinerio_pasirinkimas == "vector") {
         testuoti<vector<Studentas>>(failai, irasai, skaiciavimo_metodas, "vector", strategija);
